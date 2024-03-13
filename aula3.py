228bf847b537cd9647c72b8e1401639a48ef44d8
Wendi Ramos <81653845+WendiRamos@users.noreply.github.com>
2024-03-13 14:45:24 -0300

    feat: introducao aos tipos de dados

---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Wendi Ramos')
+print(1, 'Wendi "Ramos"')
+
+# Aspas duplas
+print("Wendi Ramos")
+print(2, "Wendi 'Ramos'")
+
+# Escape
+print("Wendi \"Ramos\"")
+
+# r
+print(r"Wendi \"Ramos\"")
