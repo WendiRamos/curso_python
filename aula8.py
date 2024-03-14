11b933c5cf1bf86305030a24f0d7228f5eb4468a
Wendi Ramos <81653845+WendiRamos@users.noreply.github.com>
2024-03-14 19:41:12 -0300

    feat: exercicio com variaveis e tipos de dados

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Wendi'
+sobrenome = 'Ramos'
+idade = 24
+ano_nascimento = 1999
+maior_de_idade = idade >= 18
+altura = 1.58
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade:", maior_de_idade)
+print("Altura em metros:", altura)
