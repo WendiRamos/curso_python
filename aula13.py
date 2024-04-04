1a2eabbc9ff5637ef967d1eed0e4d990d04a98ee
Wendi Ramos <81653845+WendiRamos@users.noreply.github.com>
2024-04-04 15:07:53 -0300

    feat: uma introducao as f-strings

---
+++ aula13.py
@@ -0,0 +1,13 @@
+nome = 'Wendi'
+altura = 1.60
+peso = 70
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
