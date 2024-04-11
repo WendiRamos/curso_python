9de4b048030ba11e303ef9beacb75e2a449b240b
Wendi Ramos <81653845+WendiRamos@users.noreply.github.com>
2024-04-11 17:48:23 -0300

    refact: atualizando codigos com o numero de aula certa

--- aula022.py
+++ aula022.py
@@ -1,7 +1,24 @@
-# Operador lógico "not"
-# Usado para inverter expressões
-# not True = False
-# not False = True
-# senha = input('Senha: ')
-print(not True)  # False
-print(not False)  # True
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# or - Qualquer condição verdadeira avalia
+# toda a expressão como verdadeira.
+# Se qualquer valor for considerado verdadeiro,
+# a expressão inteira será avaliada naquele valor.
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
+
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+
+# if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Avaliação de curto circuito
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
