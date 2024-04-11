02d8654bcb3e08951d86091ae2e25fdfc66d86b2
Wendi Ramos <81653845+WendiRamos@users.noreply.github.com>
2024-04-11 17:49:02 -0300

    feat: operadores "in" e "not in"

---
+++ aula024.py
@@ -0,0 +1,21 @@
+# Operadores in e not in
+# Strings são iteráveis
+#  0 1 2 3 4 5
+#  W e n d i i 
+# -6-5-4-3-2-1
+# nome = 'Wendi'
+# print(nome[2])
+# print(nome[-4])
+# print('dii' in nome)
+# print('zero' in nome)
+# print(10 * '-')
+# print('dii' not in nome)
+# print('zero' not in nome)
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
